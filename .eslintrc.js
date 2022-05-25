00f899bf1a5257c236ed9a86eb0786a71f5fcd13
Chmalo <chmalo.f@gmail.com>
2022-05-25 10:15:18 -0400

    feat: installing dependecies v3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,20 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard',
-    "prettier"
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["plugin:react/recommended", "standard", "prettier"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
+  plugins: ["react", "@typescript-eslint"],
   rules: {
-    "indent": "error"
-  }
+    "react/react-in-jsx-scope": "off",
+    indent: "error",
+  },
 }
--- README.md
+++ README.md
@@ -2,10 +2,16 @@
 
 React 18 Skeleton applying vite, vitest and typescript
 
+# Installation
+    npm install
+
+# Start project
+    npm run dev
+
+# Run project tests 
+    npm run test
 
 
-
-Falta por instalar
-
-react router 
-testing library
+# Notes
+"@testing-library/react-hooks" no se utiliza en react 18, la API renderHook 
+se integro directamente en "@testing-library/react".
